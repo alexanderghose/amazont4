882ac38b841ce9d1b5c5914ca2ee80de13f33cb1
alex <alex.ghose@generalassemb.ly>
2020-10-15 13:32:13 +0200

    move response and array display logic to controller instead of router

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var ProductsModel = require('../models/products.js') // import from models/products.js file
+let productCtrl = require('../controllers/products.js')
 
 /* GET products listing. */
-router.get('/', function(req, res, next) {
-  let products = ProductsModel.getProducts() // execute a function from the imported file, store the result in a variable
-  res.send('welcome to amazon. our products are:' + products);
-});
+router.get('/', productCtrl.index);
 
 module.exports = router;
